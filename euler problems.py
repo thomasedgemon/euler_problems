d494014c70ab5f3721327e1d43229bc0b40d691d
Thomas Edgemon <44883486+thomasedgemon@users.noreply.github.com>
2023-08-24 16:27:56 -0500

    euler problems.py

--- euler problems.py
+++ euler problems.py
@@ -1,5 +1,4 @@
 #find sum of even-valued fibonacci terms less than or equal to four million. 
-  
 fibonacci = [1,2,3,5]
 fibonacci_even = []
   
@@ -11,3 +10,37 @@ for n in fibonacci:
     fibonacci_even.append(n)
 
 print(sum(fibonacci_even))
+_________________________________________________________________________
+#what is the largest prime factor of 600851475143?
+
+from math import isqrt
+factors_of_n = []
+prime_factors = []
+composite_factors = []
+bignum = 600851475143
+bignum_root = isqrt(bignum)
+#find factors and reverse sort
+for x in range(1, bignum_root - 1):
+      if bignum % x == 0:
+        factors_of_n.append(x)
+        factors_of_n.sort(reverse=True) #reverse-sorted list of bignum factors
+ 
+def is_prime():
+  for n in factors_of_n:
+    prime = True
+    for i in range(2, isqrt(n)+1):
+      if n % i == 0:
+        prime = False
+        break
+    if prime:
+      if n not in prime_factors:
+        prime_factors.append(n)
+
+          
+is_prime()
+
+print("factors are")
+print(factors_of_n)    
+print("prime factors are")
+print(prime_factors)
+_________________________________________________
